28ff700affeff03bc5a4539dd4301aa8a575c12e
Timur Shakirov <shakirov.timur@gmail.com>
2016-05-09 14:40:57 +0600

    добавил шаблонныйй веб-сервер (tcp listener)

---
+++ HouseControl/WebServer/Server.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WebServer
+{
+    public class Server : ServiceBase
+    {
+        readonly TcpListener _listener;
+
+        private Server(int port)
+        {
+            var maxThreadsCount = Environment.ProcessorCount * 4;
+            ThreadPool.SetMaxThreads(maxThreadsCount, maxThreadsCount);
+            ThreadPool.SetMinThreads(1, 1);
+
+            _listener = new TcpListener(IPAddress.Any, port);
+            _listener.Start();
+
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadManager.ClientThread), _listener.AcceptTcpClient());
+            }
+        }
+
+        ~Server()
+        {
+            _listener?.Stop();
+        }
+
+        static void Main(string[] args)
+        {
+            new Server(63);
+        }
+    }
+}
---
+++ HouseControl/WebServer/ThreadPool.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+namespace WebServer
+{
+    internal static class ThreadManager
+    {
+        public static void ClientThread(object stateInfo)
+        {
+            new ClientDispatcher((TcpClient)stateInfo);
+        }
+    }
+}
