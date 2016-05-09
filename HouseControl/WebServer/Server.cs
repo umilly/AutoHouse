95c13a0572916888ca8fe058a0bb27bd2d3d65d7
Timur Shakirov <shakirov.timur@gmail.com>
2016-05-09 19:15:29 +0600

    исправил веб-версер, удалил финализатор, принимаю порт в качестве первого аргумента к консольному приложению

--- HouseControl/WebServer/Server.cs
+++ HouseControl/WebServer/Server.cs
@@ -8,31 +8,28 @@ namespace WebServer
 {
     public class Server : ServiceBase
     {
-        readonly TcpListener _listener;
-
         private Server(int port)
         {
             var maxThreadsCount = Environment.ProcessorCount * 4;
             ThreadPool.SetMaxThreads(maxThreadsCount, maxThreadsCount);
             ThreadPool.SetMinThreads(1, 1);
+            var listener = new TcpListener(IPAddress.Any, port);
 
-            _listener = new TcpListener(IPAddress.Any, port);
-            _listener.Start();
+            listener.Start();
 
             while (true)
             {
-                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadManager.ClientThread), _listener.AcceptTcpClient());
+                ThreadPool.QueueUserWorkItem(ThreadManager.ClientThread, listener.AcceptTcpClient());
             }
-        }
-
-        ~Server()
-        {
-            _listener?.Stop();
         }
 
         static void Main(string[] args)
         {
-            new Server(63);
+            int port;
+            if (int.TryParse(args[0], out port))
+            {
+                var server = new Server(port);
+            }
         }
     }
 }
