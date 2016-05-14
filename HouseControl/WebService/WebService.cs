9008312eeca4a3ee6d4d387857bcb5d67af80ef9
Timur Shakirov <shakirov.timur@gmail.com>
2016-05-15 01:42:07 +0600

    вынес в отдельную библиотеку логику TcpListener'a, создал проект WebService (Windows service) который используя WebServer - начинает слушать порт (пока прописанный в коде из сервиса)

---
+++ HouseControl/WebService/WebService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceProcess;
+using WebServer;
+
+namespace WebService
+{
+    public partial class WebService : ServiceBase
+    {
+        private Server _server;
+        public WebService()
+        {
+            InitializeComponent();
+            _server = new Server();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                _server.Start(80);//TODO configuration?
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                _server.Stop();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
